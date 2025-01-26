b95adaf17c62ea7071edabcddd9455fc76955528
LuisNunesH <luis.nunes@sptech.school>
2025-01-26 11:33:22 -0300

    feat: CORS

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from ml_model import analyze_trends, predict_total
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class TransactionData(BaseModel):
     user_id: int
